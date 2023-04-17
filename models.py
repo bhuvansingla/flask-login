81b7124fcb620fa4c7f4f24dbfecc36e581fe8c4
waltpianist97 <walterbrnrd@gmail.com>
2023-04-18 00:02:09 +0200

    now trips are added to each user and display details in separate page

--- models.py
+++ models.py
@@ -9,6 +9,8 @@ class User(db.Model,UserMixin):
     email = db.Column(db.String(140))
     password = db.Column(db.String(140))
     password_hash = db.Column(db.String(140))
+    trips = db.relationship('Trip',backref='user',lazy='dynamic')
+  
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
 
@@ -17,3 +19,17 @@ class User(db.Model,UserMixin):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Trip(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    tripname = db.Column(db.String(140))
+    speed = db.Column(db.Float(precision=2),nullable=False)
+    distance = db.Column(db.Float(precision=2),nullable=False)
+    elevation = db.Column(db.Float(precision=2),nullable=False)
+    prestige = db.Column(db.Integer,nullable=False)
+    description = db.Column(db.String(140))
+    recorded_on = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    score = db.Column(db.Float(precision=2),default=0.0)
+    def __repr__(self):
+        return '<Trip {}>'.format(self.description)
